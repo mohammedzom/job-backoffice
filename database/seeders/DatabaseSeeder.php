7f1a301a04b9097b9a36293a1be54f72fcbe7ee3
mohammedzom <mohammedzomlot2@gmail.com>
2026-03-31 14:26:38 +0300

    feat: implement full authentication and user profile management scaffolding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Companies;
+use App\Models\JobApplications;
 use App\Models\JobCategories;
 use App\Models\JobVacancies;
+use App\Models\Resumes;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -61,5 +63,52 @@ class DatabaseSeeder extends Seeder
                 'technologies' => json_encode($jobVacancy['technologies']),
             ]);
         }
+
+        $jobApplicationsData = json_decode(file_get_contents(database_path('./data/job_applications.json')), true);
+
+        foreach ($jobApplicationsData['jobApplications'] as $jobApplication) {
+            $applicantUser = User::firstOrCreate(
+                ['email' => fake()->unique()->safeEmail()],
+                [
+                    'name' => fake()->name(),
+                    'password' => Hash::make('12345678@'),
+                    'role' => 'job_seeker',
+                    'email_verified_at' => now(),
+                ]
+            );
+            $jobVacancyIds = JobVacancies::inRandomOrder()->first()->id;
+
+            $resume_data = $jobApplication['resume'];
+            $resume = Resumes::firstOrCreate([
+                'file_name' => $resume_data['filename'],
+            ], [
+                'file_url' => $resume_data['fileUri'],
+                'contact_details' => $resume_data['contactDetails'],
+                'summary' => $resume_data['summary'],
+                'skills' => $resume_data['skills'],
+                'experience' => $resume_data['experience'],
+                'education' => $resume_data['education'],
+                'user_id' => $applicantUser->id,
+            ]);
+
+            JobApplications::firstOrCreate([
+                'resume_id' => $resume->id,
+            ], [
+                'status' => $jobApplication['status'],
+                'ai_generated_score' => $jobApplication['aiGeneratedScore'],
+                'ai_generated_feedback' => $jobApplication['aiGeneratedFeedback'],
+                'job_id' => $jobVacancyIds,
+                'user_id' => $applicantUser->id,
+            ]);
+        }
+
+        /**
+         *    "resume": {
+         *        "summary": "Experienced frontend developer with 5+ years of experience building responsive and interactive user interfaces. Proficient in React.js, TypeScript, and modern CSS frameworks.",
+         *        "skills": "React.js, TypeScript, JavaScript, HTML5, CSS3, Tailwind CSS, Redux, Next.js, Git, Responsive Design",
+         *        "experience": "Senior Frontend Developer at WebTech Inc. (2020-Present)\n- Led development of company's main web application\n- Implemented responsive designs and improved performance\n- Mentored junior developers\n\nFrontend Developer at Digital Solutions (2018-2020)\n- Developed and maintained multiple client websites\n- Collaborated with backend team on API integration\n- Implemented UI/UX improvements based on user feedback",
+         *        "education": "Bachelor of Science in Computer Science, University of Technology (2014-2018)\n- Graduated with honors\n- Relevant coursework: Web Development, UI/UX Design, Database Systems"
+         *    }
+         * */
     }
 }
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,70 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+            <div class="relative" x-data="{ showPassword: false }">
+                <x-text-input id="password" class="block mt-1 w-full" name="password" required
+                    autocomplete="current-password" x-bind:type="showPassword ? 'text' : 'password'" />
+
+                <button type="button" @click="showPassword = !showPassword"
+                    class="absolute inset-y-0 right-2 flex items-center text-gray-500">
+
+                    {{-- Open Eye --}}
+                    <svg x-show="showPassword" class="w-5 h-5" width="800px" height="800px" viewBox="0 0 24 24"
+                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M4 12C4 12 5.6 7 12 7M12 7C18.4 7 20 12 20 12M12 7V4M18 5L16 7.5M6 5L8 7.5M15 13C15 14.6569 13.6569 16 12 16C10.3431 16 9 14.6569 9 13C9 11.3431 10.3431 10 12 10C13.6569 10 15 11.3431 15 13Z"
+                            stroke="#464455" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+
+                    {{-- Closed Eye --}}
+
+                    <svg x-show="!showPassword" class="w-5 h-5" width="800px" height="800px" viewBox="0 0 24 24"
+                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4 10C4 10 5.6 15 12 15M12 15C18.4 15 20 10 20 10M12 15V18M18 17L16 14.5M6 17L8 14.5"
+                            stroke="#464455" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </button>
+            </div>
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox"
+                    class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                    name="remember">
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
